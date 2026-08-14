81f9ea95cac9954bd3cbd0e7ec51e5ff2085ead7
DLP <32283725+kbst9@users.noreply.github.com>
2026-08-14 15:06:42 -0700

    Remove flattened root copies from the upload

--- env.ts
+++
@@ -1,8 +0,0 @@
-export interface Env {
-  DB: D1Database;
-  MEDIA: R2Bucket;
-  CONFIG: KVNamespace;
-  BRIDGE_BASE_URL: string;
-  CF_ACCESS_CLIENT_ID?: string;
-  CF_ACCESS_CLIENT_SECRET?: string;
-}
